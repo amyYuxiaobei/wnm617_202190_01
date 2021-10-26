ff4ecff84559ddbf9bd3d0f6a25fa94b246a674f
Xiaobei Yu <xiaobeiy@amazon.com>
2021-10-25 17:49:37 -0700

    Add information section in animal details page

--- yu.xiaobei/js/app.js
+++ yu.xiaobei/js/app.js
@@ -59,3 +59,12 @@ $(()=>{
    }[location.hash]).addClass("active");
 
 });
+
+function openCity(cityName) {
+   var i;
+   var x = document.getElementsByClassName("city");
+   for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+   }
+   document.getElementById(cityName).style.display = "block";  
+ }
