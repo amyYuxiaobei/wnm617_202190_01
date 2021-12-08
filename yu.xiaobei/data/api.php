b412f2202463523c8e7722956d55889915c30d13
Xiaobei Yu <xiaobeiy@amazon.com>
2021-12-07 16:12:23 -0800

    module 13 work

--- yu.xiaobei/data/api.php
+++ yu.xiaobei/data/api.php
@@ -45,6 +45,19 @@ function makeQuery($c,$ps,$p,$makeResults=true) {
    }
 }
 
+function makeUpload($file,$folder) {
+   $filename = microtime(true) . "_" . $_FILES[$file]['name'];
+
+   if(@move_uploaded_file(
+      $_FILES[$file]['tmp_name'],
+      $folder.$filename
+   )) return ['result'=>$filename];
+   else return [
+      "error"=>"File Upload Failed",
+      "_FILES"=>$_FILES,
+      "filename"=>$filename
+   ];
+}
 
 
 function makeStatement($data) {
@@ -54,12 +67,6 @@ function makeStatement($data) {
       $p = @$data->params;
 
       switch($t) {
-         // case "users_all":
-         //    return makeQuery($c,"SELECT * FROM `track_users`",$p);
-         // case "animals_all":
-         //    return makeQuery($c,"SELECT * FROM `track_animals`",$p);
-         // case "locations_all":
-         //    return makeQuery($c,"SELECT * FROM `track_locations`",$p);
 
          // CRUD 
 
@@ -178,6 +185,61 @@ function makeStatement($data) {
                ",$p,false);
             return ["result" => "success"];
 
+         case "update_user_onboard":
+            $r = makeQuery($c,"UPDATE
+               `track_202190_users`
+               SET
+                  `name` = ?,
+                  `img` = ?
+               WHERE `id` = ?
+               ",$p,false);
+            return ["result" => "success"];
+
+         case "update_animal_image":
+            $r = makeQuery($c,"UPDATE
+               `track_animals`
+               SET `img` = ?
+               WHERE `id` = ?
+               ",$p,false);
+            return ["result" => "success"];
+   
+
+         case "update_user_image":
+            $r = makeQuery($c,"UPDATE
+               `track_users`
+               SET `img` = ?
+               WHERE `id` = ?
+               ",$p,false);
+            return ["result" => "success"];
+   
+
+         /* SEARCH */
+         case "search_animals":
+            $p = ["%$p[0]%",$p[1]];
+            return makeQuery($c,"SELECT *
+               FROM `track_animals`
+               WHERE
+                  `name` LIKE ? AND
+                  `user_id` = ?
+               ",$p);
+
+         case "filter_animals":
+            return makeQuery($c,"SELECT *
+               FROM `track_animals`
+               WHERE
+                  `$p[0]` = ? AND
+                  `user_id` = ?
+               ",[$p[1],$p[2]]);
+         
+         /* DELETE */
+         case "delete_animal":
+            $r = makeQuery($c,"DELETE FROM `track_animals` WHERE `id` = ?",$p,false);
+            return ["result" => "success"];
+
+         case "delete_location":
+            $r = makeQuery($c,"DELETE FROM `track_locations` WHERE `id` = ?",$p,false);
+            return ["result" => "success"];
+
 
          default: return ["error"=>"No Matched Type"];
       }
@@ -186,7 +248,10 @@ function makeStatement($data) {
    }
 }
 
-
+if(!empty($_FILES)) {
+   $r = makeUpload("image","../uploads/");
+   die(json_encode($r));
+}
 $data = json_decode(file_get_contents("php://input"));
 
 die(
